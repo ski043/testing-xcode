af8796f85839f1178cd37346ba057b3e81046c2c
Jan Niklas Marzahl <janniklasmarzahl@gmail.com>
2022-01-09 19:02:36 +0100

    Initial Commit

---
+++ Todolist App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todolist App
+//
+//  Created by Jan Niklas Marzahl on 09.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todolist App/Todolist_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todolist_AppApp.swift
+//  Todolist App
+//
+//  Created by Jan Niklas Marzahl on 09.01.22.
+//
+
+import SwiftUI
+
+@main
+struct Todolist_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
